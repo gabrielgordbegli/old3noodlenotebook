d6d5234bdc424b8b85b1fa3a866bfd223f51e818
gabrielgordbegli <ggordbegli@gmail.com>
2022-11-05 03:19:19 -0400

    save

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <a href="." className={styles.card}>
+          <a href="https://nnstopgap.vercel.app/" className={styles.card}>
             <h2>PDF Merge &rarr;</h2>
             <p>Merge all your slides and control + f them all at once.</p>
           </a>
