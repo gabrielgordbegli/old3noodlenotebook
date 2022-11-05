445428f8937f7da610af6f7c9a8b54d8e040ac25
gabrielgordbegli <ggordbegli@gmail.com>
2022-11-05 03:10:45 -0400

    save

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,10 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <a href="/merge.html" className={styles.card}>
+          <a href="." className={styles.card}>
             <h2>PDF Merge &rarr;</h2>
             <p>Merge all your slides and control + f them all at once.</p>
           </a>
-
         </div>
       </main>
 
